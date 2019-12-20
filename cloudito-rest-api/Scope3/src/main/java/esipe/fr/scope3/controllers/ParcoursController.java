70677e7b82fdd6c1c51e861922c3f130a6401eec
loisguillet <lois.guillet@gmail.com>
2019-12-20 14:22:18 +0100

    start test scope3

---
+++ cloudito-rest-api/Scope3/src/main/java/esipe/fr/scope3/controllers/ParcoursController.java
@@ -0,0 +1,25 @@
+package esipe.fr.scope3.controllers;
+
+
+import esipe.fr.notification.entities.Notification;
+import esipe.fr.notification.services.NotificationService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.InetAddress;
+import java.util.List;
+
+@RestController
+@Api(tags = "Parcours")
+public class ParcoursController {
+
+
+    @RequestMapping(value = "/parcours", method = RequestMethod.GET)
+    @ApiOperation(value=" renvois le status du service")
+    @ResponseBody
+    public String chkNotifService() { return "Bonjour Parcours Service is ON.";}
+
+
+}
