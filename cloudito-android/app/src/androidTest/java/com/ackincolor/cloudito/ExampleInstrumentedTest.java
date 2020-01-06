1a499090c886841116573354c8ee78edf5418d57
loisguillet <lois.guillet@gmail.com>
2020-01-06 11:53:06 +0100

    add uuid

--- cloudito-android/app/src/androidTest/java/com/ackincolor/cloudito/ExampleInstrumentedTest.java
+++ cloudito-android/app/src/androidTest/java/com/ackincolor/cloudito/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.ackincolor.cloudito", appContext.getPackageName());
+        assertEquals("com.ackincolor.testbottomnav", appContext.getPackageName());
     }
 }
