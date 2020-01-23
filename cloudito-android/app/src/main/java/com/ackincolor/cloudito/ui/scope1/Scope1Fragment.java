137c9bd83f20d6ab58056753064de151003fa740
Térence <terencewodling@gmail.com>
2020-01-23 20:46:19 +0100

    RTT MANAGER ADDED Use of access points to call ranging requests Getting Distance / MAC ADRESS / Signal power / STATUS

--- cloudito-android/app/src/main/java/com/ackincolor/cloudito/ui/scope1/Scope1Fragment.java
+++ cloudito-android/app/src/main/java/com/ackincolor/cloudito/ui/scope1/Scope1Fragment.java
@@ -1,12 +1,21 @@
 package com.ackincolor.cloudito.ui.scope1;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.MacAddress;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.net.wifi.rtt.RangingRequest;
+import android.net.wifi.rtt.RangingResult;
+import android.net.wifi.rtt.RangingResultCallback;
+import android.net.wifi.rtt.WifiRttManager;
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +23,7 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -26,11 +36,14 @@ public class Scope1Fragment extends Fragment {
 
     private Scope1ViewModel scope1ViewModel;
 
-    // scan access points
+    // Managers
+    private WifiRttManager wifiRttManager;
     private WifiManager wifiManager;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    // ACCESS POINTS RESULTS
+    private List<ScanResult> mScanResults;
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         scope1ViewModel =
                 ViewModelProviders.of(this).get(Scope1ViewModel.class);
         View root = inflater.inflate(R.layout.fragment_scope_1, container, false);
@@ -42,54 +55,98 @@ public class Scope1Fragment extends Fragment {
             }
         });
 
-        //start Scanning
-        scanAccessPoints();
+        // WIFI MANAGE SCAN
+        this.wifiManager = (WifiManager) getContext().getSystemService(Context.WIFI_SERVICE);
+        //wifiManager.startScan();
+        mScanResults = this.wifiManager.getScanResults();
+
+        // LOG
+        Log.d("DEBUG GEOLOCATION","SCANS IS EMPTY : " + mScanResults.isEmpty());
+
+        //start Scanning with RTT
+        scanAccessPointsRTT();
 
         return root;
     }
 
-    private void scanAccessPoints(){
-        this.wifiManager = (WifiManager) getContext().getSystemService(Context.WIFI_SERVICE);
+    // CHECK IF RTT IS WORKING ON THIS DEVICE THEN -> rttManagerAvailable()
+    private void scanAccessPointsRTT() {
+        boolean rttActive = getContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_WIFI_RTT);
+        if (rttActive) {
+            Log.d("DEBUG GEOLOCATION", "RTT IS WORKING");
+            rttManagerAvailable();
+        } else {
+            Log.d("DEBUG GEOLOCATION", "RTT IS NOT WORKING");
+        }
+    }
 
-        BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
+    // CHECK IF RTT IS AVAILABLE THEN -> sendRequest()
+    private void rttManagerAvailable() {
+        this.wifiRttManager = (WifiRttManager) getContext().getSystemService(Context.WIFI_RTT_RANGING_SERVICE);
+
+        IntentFilter filter = new IntentFilter(WifiRttManager.ACTION_WIFI_RTT_STATE_CHANGED);
+        BroadcastReceiver myReceiver = new BroadcastReceiver() {
             @Override
-            public void onReceive(Context c, Intent intent) {
-                boolean success = intent.getBooleanExtra(
-                        WifiManager.EXTRA_RESULTS_UPDATED, false);
-                if (success) {
-                    scanSuccess();
+            public void onReceive(Context context, Intent intent) {
+                Log.d("DEBUG GEOLOCATION", " RTT RECEIVED");
+                if (wifiRttManager.isAvailable()) {
+                    Log.d("DEBUG GEOLOCATION", "RTT IS AVAILABLE");
+                    sendRequest();
                 } else {
-                    // scan failure handling
-                    scanFailure();
+                    Log.d("DEBUG GEOLOCATION", "RTT IS UNAVAILABLE ");
                 }
             }
         };
+        getContext().registerReceiver(myReceiver, filter);
+    }
 
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
-        getContext().registerReceiver(wifiScanReceiver, intentFilter);
+    // SEND REQUEST WITH ACCESS POINT PREVIOUSLY GOT TO GET RANGING LOCATION
+    private void sendRequest() {
+        // BUILD REQUEST TO GET RANGING OF ACCESS POINTS
 
-        boolean success = wifiManager.startScan();
-        if (!success) {
-            // scan failure handling
-            scanFailure();
+        RangingRequest.Builder builder = new RangingRequest.Builder();
+        //builder.addAccessPoint(ap1ScanResult);
+        //builder.addAccessPoint(ap2ScanResult);
+        //builder.addAccessPoint(ap3ScanResult);
+
+        RangingRequest request = builder.build();
+
+        if (ContextCompat.checkSelfPermission(this.getActivity(),Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
         }
 
-    }
+        // GETTING RANGING WITH RTT
+        wifiRttManager.startRanging(request, getContext().getMainExecutor(), new RangingResultCallback() {
 
+            @Override
+            public void onRangingFailure(int code) {
+                Log.d("DEBUG GEOLOCATION","RTT RANGING FAILURE");
+            }
 
-    private void scanSuccess() {
-        List<ScanResult> results = wifiManager.getScanResults();
-        // ... use new scan results ...
-        System.out.println("MAC : "+results.get(0).BSSID);
-        System.out.println("NETWORK NAME : "+results.get(0).SSID);
-        System.out.println("LEVEL : "+results.get(0).level);
-    }
+            @Override
+            public void onRangingResults(List<RangingResult> results) {
+                Log.d("DEBUG GEOLOCATION","RTT RANGING RESULTS");
+                for(RangingResult result : results){
+                    // MAC
+                    MacAddress mac  = result.getMacAddress();
 
-    private void scanFailure() {
-        // handle failure: new scan did NOT succeed
-        // consider using old scan results: these are the OLD results!
-        List<ScanResult> results = wifiManager.getScanResults();
-        //... potentially use older scan results ...
+                    // distance VALIDE SI status = STATUS_SUCCESS
+                    int distance = result.getDistanceMm();
+
+                    // status DISTANCE CALCULEE OK OU KO
+                    int status = result.getStatus();
+
+                    // rssi (PUISSANCE SIGNAL)
+                    int rssi = result.getRssi();
+                }
+            }
+        });
     }
 }
