8ba3fd0b3caa0748b7caf37f179684ae4cf70bcb
Térence <terencewodling@gmail.com>
2020-01-23 18:58:03 +0100

    Test getting AccessPOints

--- cloudito-android/app/src/main/java/com/ackincolor/cloudito/ui/scope1/Scope1Fragment.java
+++ cloudito-android/app/src/main/java/com/ackincolor/cloudito/ui/scope1/Scope1Fragment.java
@@ -1,5 +1,11 @@
 package com.ackincolor.cloudito.ui.scope1;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,9 +20,14 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.ackincolor.cloudito.R;
 
+import java.util.List;
+
 public class Scope1Fragment extends Fragment {
 
     private Scope1ViewModel scope1ViewModel;
+
+    // scan access points
+    private WifiManager wifiManager;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +41,55 @@ public class Scope1Fragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        //start Scanning
+        scanAccessPoints();
+
         return root;
+    }
+
+    private void scanAccessPoints(){
+        this.wifiManager = (WifiManager) getContext().getSystemService(Context.WIFI_SERVICE);
+
+        BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context c, Intent intent) {
+                boolean success = intent.getBooleanExtra(
+                        WifiManager.EXTRA_RESULTS_UPDATED, false);
+                if (success) {
+                    scanSuccess();
+                } else {
+                    // scan failure handling
+                    scanFailure();
+                }
+            }
+        };
+
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        getContext().registerReceiver(wifiScanReceiver, intentFilter);
+
+        boolean success = wifiManager.startScan();
+        if (!success) {
+            // scan failure handling
+            scanFailure();
+        }
+
+    }
+
+
+    private void scanSuccess() {
+        List<ScanResult> results = wifiManager.getScanResults();
+        // ... use new scan results ...
+        System.out.println("MAC : "+results.get(0).BSSID);
+        System.out.println("NETWORK NAME : "+results.get(0).SSID);
+        System.out.println("LEVEL : "+results.get(0).level);
+    }
+
+    private void scanFailure() {
+        // handle failure: new scan did NOT succeed
+        // consider using old scan results: these are the OLD results!
+        List<ScanResult> results = wifiManager.getScanResults();
+        //... potentially use older scan results ...
     }
 }
