a118f06aa8286a8bb241237b8e2d7d36435824db
loisguillet <lois.guillet@gmail.com>
2020-01-23 09:45:22 +0100

    test deploy

--- cloudito-android/app/src/main/java/com/ackincolor/cloudito/ui/components/Map.java
+++ cloudito-android/app/src/main/java/com/ackincolor/cloudito/ui/components/Map.java
@@ -6,8 +6,10 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
+import com.ackincolor.cloudito.entities.Coordinate;
 import com.ackincolor.cloudito.entities.Node;
 
 import java.util.ArrayList;
@@ -26,6 +28,23 @@ public class Map extends View {
         canvas.drawRect(new Rect(0,0,this.getWidth(),this.getHeight()),this.p);
     }
     public void setMap(com.ackincolor.cloudito.entities.Map map){
+        Log.d("DEBUG","setting map into view");
+        //recuperation du min Y /X et max Y/X
+        double minX=1000,minY=1000,maxX=0,maxY=0,tx=0,ty=0;
+        for(ArrayList<Coordinate> l : map.liste){
+            for(Coordinate c : l){
+                tx = c.getX();
+                ty = c.getY();
+                if(tx>maxX)
+                    maxX=ty;
+                if(tx<minX)
+                    minX = tx;
+                if(ty>maxY)
+                    maxY=ty;
+                if(ty<minY)
+                    minY=ty;
+            }
+        }
         this.map = map;
     }
 }
