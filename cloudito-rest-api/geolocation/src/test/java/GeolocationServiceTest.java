3592800dd78962fd2c0de4f5f6b2ce5c578db0f3
Jean-Michel <jm.fraullodra@gmail.com>
2020-02-02 16:28:35 +0100

    TU : test getLocation with existing user

--- cloudito-rest-api/geolocation/src/test/java/GeolocationServiceTest.java
+++ cloudito-rest-api/geolocation/src/test/java/GeolocationServiceTest.java
@@ -1,39 +1,84 @@
+import esipe.fr.geolocation.ClouditoGeolocationService;
+import esipe.fr.geolocation.exceptions.ApiException;
+import esipe.fr.geolocation.services.GeolocationService;
+import esipe.fr.model.Customer;
+import esipe.fr.model.CustomerLocation;
+import esipe.fr.model.Location;
+import esipe.fr.repositories.CustomerLocationRepository;
+import esipe.fr.repositories.CustomerRepository;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+import static org.hamcrest.Matchers.any;
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.*;
 
 
-@DataJpaTest
+@RunWith(SpringRunner.class)
 public class GeolocationServiceTest {
 
+    @TestConfiguration
+    static class EmployeeServiceImplTestContextConfiguration {
+
+        @Bean
+        public GeolocationService geolocationService() {
+            return new GeolocationService();
+        }
+    }
+
     @Autowired
-    private TestEntityManager entityManager;
+    private GeolocationService geolocationService;
 
-    @Test
-    public void getCorrectLocationTest() {
-        // When the app give a location
-        // the location is verified like the user can't be in a wall or in a hole
+    @MockBean
+    private CustomerRepository customerRepository;
+    @MockBean
+    private CustomerLocationRepository customerLocationRepository;
 
+
+    @Before
+    public void setUp() {
+        Customer jm = new Customer();
+        jm.setId(1L);
+        jm.setName("jm");
+        Location loc = new Location();
+        loc.setFloor(0);
+        loc.setX(22.2);
+        loc.setY(33.3);
+
+        CustomerLocation custoLoc = new CustomerLocation();
+        custoLoc.setCustomer(jm);
+        custoLoc.setId(1L);
+        custoLoc.setLastUpdate(new Date());
+        custoLoc.setLocation(loc);
+
+        Mockito.when(customerRepository.findById(jm.getId())).thenReturn(java.util.Optional.of(jm));
+
+        Mockito.when(customerLocationRepository.findFirstByCustomerIdOrderByLastUpdateDesc(jm.getId()))
+                .thenReturn(custoLoc);
     }
 
     @Test
-    public void setCorrectLocationTest() {
-        // When the app give a location
-        // the location is verified like the user can't be in a wall or in a hole
-
-    }
-
-    @Test
-    public void mapIsEmptyTest() {
-        // When the backend give the map
-        // the map is not empty
+    public void whenExistingUser_locationShouldBeFound() throws ApiException {
+        Long idCustomer = 1L;
+        CustomerLocation loc = geolocationService.getCustomerLocation(idCustomer);
+        assertThat(loc.getLocation().getX(),closeTo(22.2, 0.0));
+        assertThat(loc.getLocation().getY(),closeTo(33.3, 0.0));
     }
 
 
-    @Test
-    public void mapIsCompleteWithStoreAndAccessPoint() {
-        // When the backend give the map
-        // the map must be complete
-    }
 }
