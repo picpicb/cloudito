25bccc608ade568b396f83c8920fffdf7c8cd613
loisguillet <lois.guillet@gmail.com>
2020-02-02 15:22:46 +0100

    unit test in node js using mocha/chai

--- cloudito-rest-api/mappingService/app.js
+++ cloudito-rest-api/mappingService/app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const swaggerUi = require('swagger-ui-express');
+const  swaggerDocument  = require( './swagger.ts');
+
 var app = express();
 
 // view engine setup
@@ -21,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
