883eca620f8ff54644efc4285b307d7176145b41
Clement ALDOBRANDI <clement.aldobrandi@gmail.com>
2020-02-07 10:34:05 +0100

    fonctionnement ok implementation Scope 1+3

--- cloudito-android/app/src/main/java/com/ackincolor/cloudito/ui/scope4/Scope4Fragment.java
+++ cloudito-android/app/src/main/java/com/ackincolor/cloudito/ui/scope4/Scope4Fragment.java
@@ -1,6 +1,8 @@
 package com.ackincolor.cloudito.ui.scope4;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +16,8 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.ackincolor.cloudito.CourseService.CourseCache.CourseManager;
 import com.ackincolor.cloudito.CourseService.CourseInterface.CourseRetrofitController;
+import com.ackincolor.cloudito.CourseService.CourseInterface.CourseService;
+import com.ackincolor.cloudito.GeolocationService.GeolocationAndroidService;
 import com.ackincolor.cloudito.R;
 import com.ackincolor.cloudito.controllers.ParcoursController;
 import com.ackincolor.cloudito.entities.CourseNode;
@@ -23,7 +27,7 @@ import com.ackincolor.cloudito.ui.components.Map;
 import java.util.ArrayList;
 import java.util.UUID;
 
-public class Scope4Fragment extends Fragment {
+public class Scope4Fragment extends Fragment implements CourseService<ArrayList<CourseNode>> {
 
     private Scope4ViewModel scope4ViewModel;
     private CourseRetrofitController courseRetrofitController;
@@ -37,17 +41,25 @@ public class Scope4Fragment extends Fragment {
         this.courseRetrofitController = new CourseRetrofitController(new CourseManager(getContext()));
         this.courseRetrofitController.getStoresMap(mapComponent);
 
+        GeolocationAndroidService gas = new GeolocationAndroidService(getContext());
 
-        this.courseRetrofitController.getCourseNodesBtwAandB(mapComponent,3085,1710);
+            gas.recordLocation();
+          //gas.startSendLocation();
 
-        ArrayList<CourseNode> test = new ArrayList<CourseNode>();
-        test = mapComponent.getCourseNode();
-        System.out.println(test);
 
-        System.out.println(test.get(0));
+        //getCustomerLocation
+        this.courseRetrofitController.getCourseNodesBtwAandB(mapComponent,3085,1710,this);
+
         //mapComponent.mScaleFactor = 10f;
         //System.out.println(mapComponent.courseNode);
 
         return root;
+    }
+
+    @Override
+    public void onResponse(ArrayList<CourseNode> response) {
+        System.out.println(response.get(0));
+
+
     }
 }
