6fb6674c438cdb792c2ccd2b9d1ded400fe84392
loisguillet <lois.guillet@gmail.com>
2020-02-11 10:40:26 +0100

    fiz zoom in map

--- cloudito-android/app/src/main/java/com/ackincolor/cloudito/ui/components/Map3D.java
+++ cloudito-android/app/src/main/java/com/ackincolor/cloudito/ui/components/Map3D.java
@@ -1,6 +1,7 @@
 package com.ackincolor.cloudito.ui.components;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -8,12 +9,15 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 
+import com.ackincolor.cloudito.R;
 import com.ackincolor.cloudito.entities.CourseNode;
 import com.ackincolor.cloudito.entities.Location;
 
@@ -37,6 +41,7 @@ public class Map3D extends View implements MapInterface{
     private Matrix cameraMatrix;
     private float Xrotation=30f;
     private float offsetX,offsetY;
+    private Bitmap boussole;
 
     public Map3D(Context context, AttributeSet attrs) {
         super(context,attrs);
@@ -59,31 +64,51 @@ public class Map3D extends View implements MapInterface{
         this.mGestureListener = new GestureDetector(context, new GestureListener());
         this.center = new Location(0,0,200,200);
         this.zoomRatio = 1f;
+        this.mScaleFactor = 1f;
+
+        this.boussole = getBitmap(R.drawable.boussolearrow);
+        if(this.boussole == null) {
+            //Log.d("DEBUG MAP","image non trouvé");
+        }
     }
 
     public void onDraw(Canvas canvas){
         //canvas.save();
         //this.camera.getMatrix(this.cameraMatrix);
+        canvas.save();
         canvas.translate(this.getWidth()/2,this.getHeight()/2);
         if (this.center != null) {
             this.camera.save();
             this.camera.rotateX(this.Xrotation);
+            this.camera.translate(-offsetX,offsetY,0);
             this.camera.applyToCanvas(canvas);
             this.camera.restore();
 
-            this.cameraMatrix.setRotate(-this.northAngle,0,0);
+            /*this.cameraMatrix.setRotate(-this.northAngle,0,0);
             this.cameraMatrix.preScale(this.zoomRatio,this.zoomRatio);
             this.cameraMatrix.preTranslate((float) -this.center.getX(), (float) -this.center.getY());
-            this.cameraMatrix.postTranslate((float) this.center.getX(), (float) this.center.getY());
+            this.cameraMatrix.postTranslate((float) this.center.getX(), (float) this.center.getY());*/
+            this.cameraMatrix = new Matrix();
+            this.cameraMatrix.setScale(this.zoomRatio,this.zoomRatio);
+            this.cameraMatrix.preRotate(-northAngle,(float)this.center.getX(),(float)this.center.getY());
         }
         canvas.concat(this.cameraMatrix);
-        if(this.magasins!=null)
-        for(Path p : this.magasins)
-            canvas.drawPath(p,p2);
-        if(this.courses!=null)
-        for(Path p : this.courses)
-            canvas.drawPath(p,p3);
+        if(this.magasins!=null) {
+            for(Path p : this.magasins)
+                canvas.drawPath(p,p2);
+        }
+        if(this.courses!=null) {
+            for(Path p : this.courses)
+                canvas.drawPath(p,p3);
+        }
         //canvas.restore();
+        canvas.restore();
+        if(boussole!=null) {
+            Matrix matrix = new Matrix();
+            matrix.setRotate(this.northAngle, boussole.getHeight() / 2, boussole.getWidth() / 2);
+            canvas.drawBitmap(boussole, matrix, null);
+        }
+
     }
 
     public void setMap(com.ackincolor.cloudito.entities.Map map){
@@ -163,7 +188,7 @@ public class Map3D extends View implements MapInterface{
     public boolean onTouchEvent(MotionEvent event){
         mGestureListener.onTouchEvent(event);
         analyseEvent(event);
-        camera.getMatrix(this.cameraMatrix);
+        //camera.getMatrix(this.cameraMatrix);
         invalidate();
         return true;
     }
@@ -176,12 +201,13 @@ public class Map3D extends View implements MapInterface{
             //Log.d("Rotation ~~~~~~~~~~~~~~~~~", delta_x + " ## " + delta_y + " ## " + radians + " ## "
             //        + Math.toDegrees(radians));
             if(touched) {
-                this.mScaleFactor = (float) -((this.distance - (Math.sqrt(Math.abs((Math.pow(delta_x, 2) - Math.pow(delta_y, 2))))))/this.getWidth()) +1;
+                this.mScaleFactor = (float) -((this.distance - (Math.sqrt((Math.pow(delta_x, 2)+Math.pow(delta_y, 2)))))/this.getWidth()) +1;
                 this.zoomRatio*=this.mScaleFactor;
-                this.distance = Math.sqrt(Math.abs((Math.pow(delta_x, 2) - Math.pow(delta_y, 2))));
+                Log.d("DEBUG MAP 3D","mscale = "+this.mScaleFactor+" ; zoomRatio = "+this.zoomRatio);
+                this.distance = Math.sqrt((Math.pow(delta_x, 2)+Math.pow(delta_y, 2)));
             }else{
-                this.distance = Math.sqrt(Math.abs((Math.pow(delta_x, 2) - Math.pow(delta_y, 2))));
-                this.mScaleFactor = 1f;
+                this.distance = Math.sqrt((Math.pow(delta_x, 2)+Math.pow(delta_y, 2)));
+                //this.mScaleFactor = 1f;
                 touched = true;
             }
             float deg = (float) Math.toDegrees(radians);
@@ -197,12 +223,12 @@ public class Map3D extends View implements MapInterface{
                 //Log.d("Rotation ~~~~~~~false~~~~~~~", "angle : "+this.lastRotation);
                 touched2 = true;
             }
-            invalidate();
+            //invalidate();
             return (float) Math.toDegrees(radians);
         }else if(event.getPointerCount()>=3){
             if(touched3){
                 this.realRotation = 0.0f;
-                this.mScaleFactor = 1f;
+                //this.mScaleFactor = 1f;
                 touched = false;
                 touched2 = false;
                 float y = event.getY(0);
@@ -223,7 +249,7 @@ public class Map3D extends View implements MapInterface{
             }
         }
         this.realRotation = 0.0f;
-        this.mScaleFactor = 1f;
+        //this.mScaleFactor = 1f;
         touched = false;
         touched2 = false;
         touched3 = false;
@@ -236,10 +262,20 @@ public class Map3D extends View implements MapInterface{
         public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
             offsetX+=distanceX;
             offsetY+=distanceY;
-            camera.translate(-distanceX,distanceY,0);
+            //camera.translate(-distanceX,distanceY,0);
             //Log.d("DEBUG MAP","distance x :"+offsetX+" ; distance Y : "+offsetY);
             //invalidate();
             return true;
         }
+    }
+    private Bitmap getBitmap(int drawableRes) {
+        Drawable drawable = getResources().getDrawable(drawableRes);
+        Canvas canvas = new Canvas();
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        canvas.setBitmap(bitmap);
+        drawable.setBounds(0, 0, drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
     }
 }
