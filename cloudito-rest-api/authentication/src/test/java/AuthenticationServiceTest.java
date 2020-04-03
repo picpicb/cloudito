b9e2ca0ad43f40d0ba5da2f3dcc136e5747ff64f
Térence <terencewodling@gmail.com>
2020-04-03 10:28:40 +0200

    Prep TU Authentication

--- cloudito-rest-api/authentication/src/test/java/AuthenticationServiceTest.java
+++ cloudito-rest-api/authentication/src/test/java/AuthenticationServiceTest.java
@@ -1,3 +1,39 @@
+import esipe.fr.authentication.services.AuthenticationService;
+import esipe.fr.repositories.CustomerRepository;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 public class AuthenticationServiceTest {
+    @TestConfiguration
+    static class EmployeeServiceImplTestContextConfiguration {
+
+        @Bean
+        public AuthenticationService geolocationService() {
+            return new AuthenticationService();
+        }
+    }
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @MockBean
+    private CustomerRepository customerRepository;
+
+    @Before
+    public void setUp() {
+
+    }
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
 
 }
