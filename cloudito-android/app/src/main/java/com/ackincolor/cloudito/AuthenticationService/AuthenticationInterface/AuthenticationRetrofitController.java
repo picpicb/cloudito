38856030e86904495af094bb6975b6cb4473eec1
Térence <terencewodling@gmail.com>
2020-04-03 09:55:05 +0200

    Preparation auth via Credentials sur android

--- cloudito-android/app/src/main/java/com/ackincolor/cloudito/AuthenticationService/AuthenticationInterface/AuthenticationRetrofitController.java
+++ cloudito-android/app/src/main/java/com/ackincolor/cloudito/AuthenticationService/AuthenticationInterface/AuthenticationRetrofitController.java
@@ -1,4 +1,108 @@
 package com.ackincolor.cloudito.AuthenticationService.AuthenticationInterface;
 
+import android.content.Context;
+
+import com.ackincolor.cloudito.AuthenticationService.AuthenticationAndroidService;
+import com.ackincolor.cloudito.entities.Credentials;
+import com.google.gson.Gson;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class AuthenticationRetrofitController {
+    private Context context;
+    private AuthenticationAndroidService androidService;
+    static final String BASE_URL = "http://ackincolor.ddns.net:3086/"; //port 3083 ???
+    private Gson gson;
+    public AuthenticationRetrofitController(Context context, AuthenticationAndroidService androidService) {
+        this.context = context;
+        this.androidService = androidService;
+    }
+
+    public void authenticateLogin(Credentials crendentials){
+        this.gson = new Gson();
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(httpClient.build())
+                .addConverterFactory(GsonConverterFactory.create(this.gson))
+                .build();
+
+        AuthenticationRetrofitService service = retrofit.create(AuthenticationRetrofitService.class);
+
+        service.authenticateLogin(crendentials).enqueue(new Callback<Credentials>() {
+
+            @Override
+            public void onResponse(Call<Credentials> call, Response<Credentials> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<Credentials> call, Throwable t) {
+
+            }
+        });
+    }
+
+    public void authenticateOtpCode(Credentials crendentials){
+        this.gson = new Gson();
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(httpClient.build())
+                .addConverterFactory(GsonConverterFactory.create(this.gson))
+                .build();
+
+        AuthenticationRetrofitService service = retrofit.create(AuthenticationRetrofitService.class);
+
+        service.authenticateOtpCode(crendentials).enqueue(new Callback<Credentials>() {
+
+            @Override
+            public void onResponse(Call<Credentials> call, Response<Credentials> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<Credentials> call, Throwable t) {
+
+            }
+        });
+    }
+
+    public void authenticateInscription(Credentials crendentials){
+        this.gson = new Gson();
+        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(httpClient.build())
+                .addConverterFactory(GsonConverterFactory.create(this.gson))
+                .build();
+
+        AuthenticationRetrofitService service = retrofit.create(AuthenticationRetrofitService.class);
+
+        service.authenticateInscription(crendentials).enqueue(new Callback<Credentials>() {
+
+            @Override
+            public void onResponse(Call<Credentials> call, Response<Credentials> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<Credentials> call, Throwable t) {
+
+            }
+        });
+    }
+
 }
--- cloudito-android/app/src/main/java/com/ackincolor/cloudito/entities/Credentials.java
+++ cloudito-android/app/src/main/java/com/ackincolor/cloudito/entities/Credentials.java
@@ -1,4 +1,38 @@
 package com.ackincolor.cloudito.entities;
 
 public class Credentials {
+
+    private String usrId,login,pwd,key;
+    private boolean authenticated;
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getUsrId() {
+        return usrId;
+    }
+
+    public void setUsrId(String usrId) {
+        this.usrId = usrId;
+    }
 }
