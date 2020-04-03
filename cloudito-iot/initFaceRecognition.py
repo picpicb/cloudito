01aef4750ebc8ba08bf2567d29b1a9016b866914
Pierre Toker <pierre.toker-kovacic@etu.u-pec.fr>
2020-04-03 11:38:05 +0200

    Adding loop which detect something on the picture by changing sensibility and delete useless file.

--- cloudito-iot/initFaceRecognition.py
+++ cloudito-iot/initFaceRecognition.py
@@ -10,20 +10,36 @@ class NumpyArrayEncoder(JSONEncoder):
             return obj.tolist()
         return JSONEncoder.default(self, obj)
 
+
 img = cv2.imread('images.jpg')
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 face_haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 faces_detected = face_haar_cascade.detectMultiScale(gray_img, scaleFactor=1.32, minNeighbors=5)
 
+faces_detected = []
+scaleFactor = 1.25
+neightborsSensibility = 5
+while len(faces_detected) == 0:
+    faces_detected = face_haar_cascade.detectMultiScale(gray_img, scaleFactor=scaleFactor, minNeighbors=neightborsSensibility)
+    if scaleFactor == 1.01:
+        break
+    scaleFactor = scaleFactor - 0.01
+    if neightborsSensibility == 0:
+        neightborsSensibility = 5
+    else:
+        neightborsSensibility = neightborsSensibility - 1
+
+if len(faces_detected) == 0:
+    print("Nothing detected...")
+
 i = 0
-list = []
+outputList = []
 for face in faces_detected:
     (x, y, w, h) = face
     face_grey = gray_img[y:y + h, x:x + h]
-    numpyData = {"array": face_grey}
+    numpyData = {"i": face_grey}
     encodedNumpyData = json.dumps(numpyData, cls=NumpyArrayEncoder)  # use dump() to write array into file
-    list.append(encodedNumpyData)
+    outputList.append(encodedNumpyData)
     i = i + 1
 
-np.savez_compressed("result", a=list)
-
+np.savez_compressed("result", a=outputList)
