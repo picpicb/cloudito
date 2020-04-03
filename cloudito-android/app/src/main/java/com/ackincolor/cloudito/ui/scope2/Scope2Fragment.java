4ea2319cc12e7612a581259d9145f6f024b4728e
waniscloud2 <waniscloud2@gmail.com>
2020-04-03 19:59:02 +0200

    Authentification on Android; basic activity with limit on number of attempts

--- cloudito-android/app/src/main/java/com/ackincolor/cloudito/ui/scope2/Scope2Fragment.java
+++ cloudito-android/app/src/main/java/com/ackincolor/cloudito/ui/scope2/Scope2Fragment.java
@@ -1,9 +1,11 @@
 package com.ackincolor.cloudito.ui.scope2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,17 +15,27 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.ackincolor.cloudito.R;
+import com.ackincolor.cloudito.ui.authentification.AuthentificationFragment;
 
 public class Scope2Fragment extends Fragment {
 
     private Scope2ViewModel scope2ViewModel;
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         scope2ViewModel =
                 ViewModelProviders.of(this).get(Scope2ViewModel.class);
         View root = inflater.inflate(R.layout.fragment_scope_2, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
+        final TextView textView = root.findViewById(R.id.textViewToken);
+        Button button = (Button) root.findViewById(R.id.logTokenButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                updateDetail();
+            }
+        });
         scope2ViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
@@ -31,5 +43,10 @@ public class Scope2Fragment extends Fragment {
             }
         });
         return root;
+    }
+    public void updateDetail()
+    {
+        Intent intent = new Intent(getActivity(), AuthentificationFragment.class);
+        startActivity(intent);
     }
 }
