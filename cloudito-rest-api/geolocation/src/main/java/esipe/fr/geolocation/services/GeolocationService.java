85969fd36f66285afbea06d70c49140e4cdc788f
lois guillet <lois.guillet@gmail.com>
2020-04-29 10:02:29 +0200

    test vault connexion : OK test with Sevice Geolocation

--- cloudito-rest-api/geolocation/src/main/java/esipe/fr/geolocation/services/GeolocationService.java
+++ cloudito-rest-api/geolocation/src/main/java/esipe/fr/geolocation/services/GeolocationService.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import esipe.fr.repositories.CustomerLocationRepository;
-import esipe.fr.encryption.service.Encryption;
 
 import java.util.Date;
 import java.util.Optional;
