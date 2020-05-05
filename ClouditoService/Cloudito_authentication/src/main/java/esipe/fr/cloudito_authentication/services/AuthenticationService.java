27f18a24671c5c173c9aa5bbaaed60be7ee74da3
Térence <terencewodling@gmail.com>
2020-05-05 02:30:48 +0200

    Test date

--- ClouditoService/Cloudito_authentication/src/main/java/esipe/fr/cloudito_authentication/services/AuthenticationService.java
+++ ClouditoService/Cloudito_authentication/src/main/java/esipe/fr/cloudito_authentication/services/AuthenticationService.java
@@ -134,7 +134,7 @@ public class AuthenticationService {
 
         long timeSystem2Min = System.currentTimeMillis() +((160)*1000);
         long time2Min = ((timeSystem2Min/*+(2*3600*1000)*/) / 1000) / 30;
-        String hexTime2Min = Long.toHexString(time);
+        String hexTime2Min = Long.toHexString(time2Min);
 
         return TOTP.generateTOTP(hexKey, hexTime, "6") +"/"+TOTP.generateTOTP(hexKey, hexTime2Min, "6");
     }
