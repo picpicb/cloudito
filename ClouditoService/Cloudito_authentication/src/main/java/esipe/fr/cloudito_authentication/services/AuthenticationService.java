424e98af2aed6f2fd1ab7df05932de16af333a85
Térence <terencewodling@gmail.com>
2020-05-05 02:20:24 +0200

    Test date

--- ClouditoService/Cloudito_authentication/src/main/java/esipe/fr/cloudito_authentication/services/AuthenticationService.java
+++ ClouditoService/Cloudito_authentication/src/main/java/esipe/fr/cloudito_authentication/services/AuthenticationService.java
@@ -132,7 +132,7 @@ public class AuthenticationService {
 
         //System.out.println( new Date( (new Timestamp(timeSystem)).getTime() ) );
 
-        long timeSystem2Min = System.currentTimeMillis() +(30)*1000;
+        long timeSystem2Min = System.currentTimeMillis() +((160)*1000);
         long time2Min = ((timeSystem/*+(2*3600*1000)*/) / 1000) / 30;
         String hexTime2Min = Long.toHexString(time);
 
