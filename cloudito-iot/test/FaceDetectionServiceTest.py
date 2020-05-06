28aa5810507245fd09f6aaf4b573b9fc1be7c4ec
Michel CAI <michelbts2@gmail.com>
2020-05-06 11:19:40 +0200

    START TU

--- cloudito-iot/test/FaceDetectionServiceTest.py
+++ cloudito-iot/test/FaceDetectionServiceTest.py
@@ -1,5 +1,6 @@
 import unittest
 
+
 class FaceDetectionServiceTest(unittest.TestCase):
     """Unit test for face recognition"""
 
@@ -7,8 +8,12 @@ class FaceDetectionServiceTest(unittest.TestCase):
         import cv2
 
     def test_video_capture(self):
-
         import cv2
         cap = cv2.VideoCapture("../test.mp4")
         self.assertTrue(cap.isOpened())
+
+    def test_format(self):
+        pass
+
+
 
